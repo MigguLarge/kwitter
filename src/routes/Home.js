725c6811242112796d449ef2a220d8ff1d049724
MigguLarge <33200947+MigguLarge@users.noreply.github.com>
2020-10-21 13:13:03 +0900

    3.4 Delete and Update part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,46 @@
-import React, { useState } from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import Kweet from "components/Kweet"
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [kweet, setKweet] = useState("");
-  const onSubmit = (event) => {
+  const [kweets, setKweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("kweets").onSnapshot((snapshot) => {
+      const kweetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setKweets(kweetArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await dbService.collection("kweets").add({
+      text: kweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setKweet("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setKweet(value);
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={kweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Kweet" />
       </form>
+      <div>
+        {kweets.map((kweet) => (
+            <Kweet key={kweet.id} kweetObj={kweet} isOwner={kweet.creatorId === userObj.uid} />
+        ))}
+      </div>
     </div>
   );
 };
